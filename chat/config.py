9a58788a46d53cf2818a2bfb09bb58c24ca3ab33
unknown <aarshad11199@gmail.com>
2024-10-31 22:50:27 +0530

    create chat app

---
+++ chat/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_#$%@@'
+    
+    # Database configuration using the provided host, port, username, and password for MySQL
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') or
+        'mysql+pymysql://root:root@127.0.0.1:8889/chat'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
